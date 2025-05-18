75099f07a9b0842539c16b19a59a0e372241263b
Jakub Krajewski <jakubpkrajewski@gmail.com>
2025-05-18 11:24:30 +0200

    add hotreload , fix vue imports

---
+++ backend/src/moderation/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from moderation.core.settings import settings
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.moderation.app:create_app",
+        factory=True,
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
+        reload=True,
+    )
